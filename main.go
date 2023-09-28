3f9c969e03067491a2122f73689f0fb3fdd7d107
DunKy <duncan.lhuillery@ynov.com>
2023-09-28 09:46:37 +0200

    DunKy

--- main.go
+++ main.go
@@ -8,28 +8,29 @@ import (
 	"main.go/opcodes"
 )
 
-type Game struct{
+type Game struct {
 	cpu *opcodes.Cpu
 }
 
 func (g *Game) Update() error {
-    return nil
+
+	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-    ebitenutil.DebugPrint(screen, "Hello, World!")
+	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-    return 640, 320
+	return 640, 320
 }
 
 func main() {
-    ebiten.SetWindowSize(640, 320)
-    ebiten.SetWindowTitle("Chip8 Emulator")
-    ebiten.RunGame(&Game{})
-    if err := ebiten.RunGame(&Game{}); err != nil {
-        log.Fatal(err)
-    }
+	ebiten.SetWindowSize(640, 320)
+	ebiten.SetWindowTitle("Chip8 Emulator")
+	ebiten.RunGame(&Game{})
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 
 }
